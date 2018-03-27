9bf7a7991166240b41c2afcb05a527e143c3c328
mattmlz <matthieu.toussaint@hetic.net>
2018-03-27 15:58:05 +0200

    create links to css

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSS & fonts -->
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="assets/css/main.css">
   <!-- FAVICONS -->
   <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
@@ -21,6 +25,7 @@
   <title>Remember it!</title>
 </head>
 <body>
+
   
 </body>
 </html>
