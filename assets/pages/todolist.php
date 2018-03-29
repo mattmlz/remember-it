b48a45e9c93509092331394d5803fe988357dc92
mattmlz <matthieu.toussaint@hetic.net>
2018-03-29 13:05:54 +0200

    starting to display tasks without connections to DB

--- assets/pages/todolist.php
+++ assets/pages/todolist.php
@@ -1,3 +1,44 @@
 <?php
+  include '../includes/config.php';
+?>
 
-die('ok');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSS & fonts -->
+  <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,700" rel="stylesheet">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/tasks.css">
+  <!-- FAVICONS -->
+  <link rel="apple-touch-icon" sizes="180x180" href="../img/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../img/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../img/favicon/favicon-16x16.png">
+  <link rel="manifest" href="../img/favicon/site.webmanifest">
+  <link rel="mask-icon" href="../img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="../img/favicon/favicon.ico">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-config" content="../img/favicon/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
+  <title>Remember it!</title>
+</head>
+<body>
+  <div class="container">
+    <div class="tasks">
+      <table>
+        <tr>
+          <th>State</th>
+          <th>What you have to do:</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td>Wash my appartment</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+
+</body>
+</html>
